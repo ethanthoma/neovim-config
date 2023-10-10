aa1eea424f4856f6fd5813fbc445b874c872ca50
Ethan Thoma <ethanthoma@gmail.com>
2023-10-10 11:40:42 -0700

    added fugitive and removed shae

--- lua/plugins/cosmetic/rose-pine.lua
+++ lua/plugins/cosmetic/rose-pine.lua
@@ -1,7 +1,28 @@
 return {
 	'rose-pine/neovim', name = 'rose-pine', lazy = false, priority = 1000,
 	config = function()
+        require('rose-pine').setup({
+            variant = 'moon',
+            disable_background = true,
+            highlight_groups = {
+                TelescopeBorder = { fg = "highlight_high", bg = "none" },
+                TelescopeNormal = { bg = "none" },
+                TelescopePromptNormal = { bg = "base" },
+                TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+                TelescopeSelection = { fg = "text", bg = "base" },
+                TelescopeSelectionCaret = { fg = "rose", bg = "rose" },}
+        })
+
 		vim.cmd('colorscheme rose-pine')
+
+        vim.cmd [[
+            highlight Visual guibg = #56526e
+        ]]
+
+        vim.cmd [[
+            highlight LineNr guifg=#e0def4
+            highlight CursorLineNr guifg=#e0def4
+        ]]
 	end,
 }
 
--- lua/plugins/cosmetic/shade.lua
+++
@@ -1,4 +0,0 @@
-return {
-	'sunjon/shade.nvim'
-}
-
