cc710d2a59254cab15bcfbd5468870fe3520aa79
Ethan Thoma <ethanthoma@gmail.com>
2024-01-31 17:18:32 +0000

    added noice and some snippet tools

--- lua/plugins/cosmetic/lualine.lua
+++ lua/plugins/cosmetic/lualine.lua
@@ -11,8 +11,8 @@ return {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
                 lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
+                lualine_x = {'filetype'},
+                lualine_y = {},
                 lualine_z = {'location'}
             },
             inactive_sections = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,16 +11,17 @@ return {
         {'hrsh7th/nvim-cmp'},
         {'hrsh7th/cmp-buffer'},
         {'hrsh7th/cmp-path'},
-        {'saadparwaiz1/cmp_luasnip'},
         {'hrsh7th/cmp-nvim-lsp'},
         {'hrsh7th/cmp-nvim-lua'},
 
         -- Snippets
         {'L3MON4D3/LuaSnip'},
+        {'hrsh7th/cmp-vsnip'},
+        {'hrsh7th/vim-vsnip'},
         {'rafamadriz/friendly-snippets'},
+        {'saadparwaiz1/cmp_luasnip'},
 
         -- Telescope extensions
-        {'nvim-telescope/telescope-frecency.nvim'},
         {'nvim-telescope/telescope-media-files.nvim'},
     },
     config = function()
