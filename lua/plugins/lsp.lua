efc8e2735a3641d34de173753cfe10023ec5c521
Ethan Thoma <ethanthoma@gmail.com>
2024-11-08 12:20:43 -0800

    feat: updated lsps, removed mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,33 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v4.x",
     dependencies = {
         -- LSP Support
-        { 'neovim/nvim-lspconfig' },
-        { 'williamboman/mason.nvim' },
-        { 'williamboman/mason-lspconfig.nvim' },
+        { "neovim/nvim-lspconfig" },
 
-        -- Format
-        { 'lukas-reineke/lsp-format.nvim' },
+        -- Formatters
+        { "lukas-reineke/lsp-format.nvim" },
         { "stevearc/conform.nvim" },
 
+        -- Linters
+        { "mfussenegger/nvim-lint" },
+
         -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
+        { "hrsh7th/nvim-cmp" },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-path" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lua" },
 
         -- Snippets
-        { 'L3MON4D3/LuaSnip' },
-        { 'hrsh7th/cmp-vsnip' },
-        { 'hrsh7th/vim-vsnip' },
-        { 'rafamadriz/friendly-snippets' },
-        { 'saadparwaiz1/cmp_luasnip' },
+        { "L3MON4D3/LuaSnip" },
+        { "hrsh7th/cmp-vsnip" },
+        { "hrsh7th/vim-vsnip" },
+        { "rafamadriz/friendly-snippets" },
+        { "saadparwaiz1/cmp_luasnip" },
 
         -- Telescope extensions
-        { 'nvim-telescope/telescope-media-files.nvim' },
+        { "nvim-telescope/telescope-media-files.nvim" },
     },
     config = function()
         local lsp_zero = require("lsp-zero")
@@ -49,16 +50,103 @@ return {
                 vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
 
-                local id = vim.tbl_get(event, 'data', 'client_id')
+                local id = vim.tbl_get(event, "data", "client_id")
                 local client = id and vim.lsp.get_client_by_id(id)
                 if client == nil then
                     return
                 end
 
-                if client.supports_method('textDocument/formatting') then
-                    require('lsp-format').on_attach(client)
+                if client.supports_method("textDocument/formatting") then
+                    require("lsp-format").on_attach(client)
                 end
             end,
+        })
+
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = " ",
+                    [vim.diagnostic.severity.WARN] = " ",
+                    [vim.diagnostic.severity.HINT] = " ",
+                    [vim.diagnostic.severity.INFO] = " ",
+                },
+            },
+        })
+
+        -- LSPs
+
+        local servers = {
+            lua_ls = {
+                executable = "lua-language-server",
+                config = {
+                    settings = { Lua = { diagnostics = { globals = { "vim" } } } }
+                }
+            },
+            hls = {
+                executable = "haskell-language-server",
+                config = {
+                    cmd = { "haskell-language-server", "--lsp" }
+                }
+            },
+            rust_analyzer = {
+                executable = "rust-analyzer",
+                config = {}
+            },
+            harper_ls = {
+                executable = "harper-ls",
+                config = {}
+            },
+            ols = {
+                executable = "ols",
+                config = {}
+            },
+            pylyzer = {
+                executable = "pylyzer",
+                config = {}
+            },
+            ruff = {
+                executable = "ruff",
+                config = {}
+            },
+            gleam = {
+                executable = "gleam",
+                config = {}
+            },
+            ts_ls = {
+                executable = "typescript-language-server",
+                config = {}
+            }
+        }
+
+        for server_name, settings in pairs(servers) do
+            if vim.fn.executable(settings.executable) == 1 then
+                require("lspconfig")[server_name].setup(settings.config)
+            end
+        end
+
+        -- Linters
+
+        require("lint").linters_by_ft = {
+            javascript = { "oxlint" },
+        }
+
+        vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+            callback = function()
+                require("lint").try_lint()
+            end,
+        })
+
+        -- Formatters
+
+        require("conform").setup({
+            formatters_by_ft = {
+                nix = { "nixfmt" },
+                rust = { "rustfmt" },
+                gleam = { "gleam" },
+                json = { "jq" },
+                typescript = { "prettierd" },
+                javascript = { "prettierd" },
+            },
         })
 
         vim.api.nvim_create_autocmd("BufWritePre", {
@@ -68,85 +156,18 @@ return {
             end,
         })
 
-        vim.diagnostic.config({
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = ' ',
-                    [vim.diagnostic.severity.WARN] = ' ',
-                    [vim.diagnostic.severity.HINT] = ' ',
-                    [vim.diagnostic.severity.INFO] = ' ',
-                },
-            },
-        })
+        -- Snippets
 
-        require("conform").setup({
-            formatters_by_ft = {
-                nix = { "nixfmt" },
-                rust = { "rustfmt" },
-                gleam = { "gleam" },
-                json = { "jq" },
-            },
-        })
-
-        local lsp_configurations = require('lspconfig.configs')
-
-        if vim.fn.executable("lua-language-server") == 1 then
-            require("lspconfig")["lua_ls"].setup({
-                settings = { diagnostics = { globals = { "vim" } } }
-            })
-        end
-
-        if vim.fn.executable("haskell-language-server") == 1 then
-            require("lspconfig")["hls"].setup({
-                cmd = { "haskell-language-server", "--lsp" }
-            })
-        end
-
-        if vim.fn.executable("rust-analyzer") == 1 then
-            require("lspconfig")["rust_analyzer"].setup({})
-        end
-
-        if vim.fn.executable("harper-ls") == 1 then
-            require("lspconfig")["harper_ls"].setup({})
-        end
-
-        if vim.fn.executable("ols") == 1 then
-            require("lspconfig")["ols"].setup({})
-        end
-
-        if vim.fn.executable("pylyzer") == 1 then
-            require("lspconfig")["pylyzer"].setup({})
-        end
-
-        if vim.fn.executable("ruff") == 1 then
-            require("lspconfig")["ruff"].setup({})
-        end
-
-        if vim.fn.executable("gleam") == 1 then
-            require("lspconfig")["gleam"].setup({})
-        end
-
-        if vim.fn.executable("typescript-language-server") == 1 then
-            require("lspconfig")["ts_ls"].setup({})
-        end
-
-        require("mason").setup()
-        require("mason-lspconfig").setup_handlers {
-            function(server_name)
-                require("lspconfig")[server_name].setup {}
-            end,
-        }
-
-        local cmp = require('cmp')
+        local cmp = require("cmp")
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
             sources = {
-                { name = 'path' },
-                { name = 'nvim_lsp' },
-                { name = 'nvim_lua' },
-                { name = 'luasnip', keyword_length = 2 },
-                { name = 'buffer',  keyword_length = 3 },
+                { name = "path" },
+                { name = "nvim_lsp" },
+                { name = "nvim_lua" },
+                { name = "luasnip", keyword_length = 2 },
+                { name = "buffer",  keyword_length = 3 },
             },
             experimental = {
                 ghost_text = true,
@@ -158,12 +179,12 @@ return {
             },
             formatting = lsp_zero.cmp_format(),
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<Tab>'] = nil,
-                ['<S-Tab>'] = nil,
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+                ["<Tab>"] = nil,
+                ["<S-Tab>"] = nil,
             }),
         })
     end,
